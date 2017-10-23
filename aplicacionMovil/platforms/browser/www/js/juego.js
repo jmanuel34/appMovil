273d8c50e9c59ce129fb33b5b8468468aaf5b6a4
jmanuel34 <jmanuel34@gmail.com>
2017-10-23 19:11:13 +0200

    Juego-Parte-3

--- aplicacionMovil/platforms/browser/www/js/juego.js
+++ aplicacionMovil/platforms/browser/www/js/juego.js
@@ -1,7 +1,7 @@
 var app = {
   inicio: function() {
     DIAMETRO_BOLA = 50;
-    
+    dificultad = 0;
     velocidadX = 0;
     velocidadY = 0;
     puntuacion = 0;
@@ -20,21 +20,29 @@ var app = {
 
       game.stage.backgroundColor = '#f27d0c';
       game.load.image('bola', 'assets/bola.png');
+      game.load.image('objetivo', 'assets/objetivo.png')
     }
 
     function create() {
       scoreText = game.add.text(16, 16, puntuacion, { fontSize: '100px', fill: '#757676' });
+
+      objetivo = game.add.sprite(app.inicioX(), app.inicioY(), 'objetivo');
       bola = game.add.sprite(app.inicioX(), app.inicioY(), 'bola');
+
       game.physics.arcade.enable(bola);
+      game.physics.arcade.enable(objetivo);
+
       bola.body.collideWorldBounds = true;
       bola.body.onWorldBounds = new Phaser.Signal();
       bola.body.onWorldBounds.add(app.decrementaPuntuacion, this);
     }
 
     function update() {
-      bola.body.velocity.y = (velocidadY * 300);
-      bola.body.velocity.x = (velocidadX * -300);
+      var factorDificultad = (300 + (dificultad * 100));
+      bola.body.velocity.y = (velocidadY * factorDificultad);
+      bola.body.velocity.x = (velocidadX * (-1 * factorDificultad));
 
+      game.physics.arcade.overlap(bola, objetivo, app.incrementaPuntuacion, null, this);
     }
 
     var estados = { preload: preload, create: create, update: update };
@@ -44,6 +52,18 @@ var app = {
   decrementaPuntuacion: function() {
     puntuacion = puntuacion - 1;
     scoreText.text = puntuacion;
+  },
+
+  incrementaPuntuacion: function() {
+    puntuacion = puntuacion + 1;
+    scoreText.text = puntuacion;
+
+    objetivo.body.x = app.inicioX();
+    objetivo.body.y = app.inicioY();
+    
+    if (puntuacion > 0) {
+      dificultad = dificultad + 1;
+    }
   },
 
   inicioX: function() {
@@ -59,23 +79,21 @@ var app = {
   },
 
   vigilaSensores: function() {
-
     function onError() {
       console.log('onError!');
     }
-
     function onSuccess(datosAceleracion) {
       app.detectaAgitacion(datosAceleracion);
       app.registraDireccion(datosAceleracion);
     }
-
-    navigator.accelerometer.watchAcceleration(onSuccess, onError, { frequency: 10 });
+    navigator.accelerometer.watchAcceleration(onSuccess, onError, {
+      frequency: 10
+    });
   },
 
   detectaAgitacion: function(datosAceleracion) {
-    var agitacionX = datosAceleracion.x > 10;
-    var agitacionY = datosAceleracion.y > 10;
-
+    agitacionX = datosAceleracion.x > 10;
+    agitacionY = datosAceleracion.y > 10;
     if (agitacionX || agitacionY) {
       setTimeout(app.recomienza, 1000);
     }
