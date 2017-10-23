4c3c320faf876a43197c910aad48ffe42deccc21
jmanuel34 <jmanuel34@gmail.com>
2017-10-23 19:09:03 +0200

    Juego1

--- aplicacionMovil/platforms/browser/www/js/juego.js
+++ aplicacionMovil/platforms/browser/www/js/juego.js
@@ -1,6 +1,5 @@
 var app = {
   inicio: function() {
-<<<<<<< HEAD
     DIAMETRO_BOLA = 50;
     
     velocidadX = 0;
@@ -95,47 +94,5 @@ var app = {
 if ('addEventListener'in document) {
   document.addEventListener('deviceready', function() {
     app.inicio();
-=======
-
-    function onError() {
-        console.log('onError');
-    }
-
-    navigator.accelerometer.watchAcceleration(this.onSuccess, onError, { frequency: 1000});
-  },
-
-  onSuccess: function(datosAceleracion){
-    app.detectaAgitacion(datosAceleracion);
-    app.representaValores(datosAceleracion);
-  },
-
-  detectaAgitacion: function(datosAceleracion){
-    agitacionX = datosAceleracion.x > 10;
-    agitacionY = datosAceleracion.y > 10;
-
-    if (agitacionX || agitacionY){
-      document.body.className = 'agitado';
-    }else{
-      document.body.className = '';
-    }
-  },
-
-  representaValores: function(datosAceleracion){
-    app.representa(datosAceleracion.x, '#valorx');
-    app.representa(datosAceleracion.y, '#valory');
-    app.representa(datosAceleracion.z, '#valorz');
-  },
-
-  representa: function(dato, elementoHTML){
-    redondeo = Math.round(dato * 100) / 100
-    document.querySelector(elementoHTML).innerHTML= redondeo;
-  }
-
-};
-
-if ('addEventListener'in document) {
-    document.addEventListener('deviceready', function() {
-        app.inicio();
->>>>>>> Acelerometro
   }, false);
 }
