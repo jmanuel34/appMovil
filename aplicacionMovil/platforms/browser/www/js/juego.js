f03d3893ba8ef65fb9d95fd1aa1f4ad82eca5aad
jmanuel34 <jmanuel34@gmail.com>
2017-10-23 18:39:07 +0200

    Juego
    Aplicacion de juego parte 1

--- aplicacionMovil/platforms/browser/www/js/juego.js
+++ aplicacionMovil/platforms/browser/www/js/juego.js
@@ -1,26 +1,44 @@
-var app={
-    inicio: function(){
-     function onError(){
-         console.log('onError!');
-     }
-     navigator.accelerometer.watchAcceleration(this.onSuccess, onError, {frequency : 1000});
-    },
-    
-  onSuccess:function(datosAceleracion){
-      app.representa(datosAceleracion.x, '#valorx');
-      app.representa(datosAceleracion.x, '#valory');
-      app.representa(datosAceleracion.x, '#valorz');
+var app = {
+  inicio: function() {
+
+    function onError() {
+        console.log('onError');
+    }
+
+    navigator.accelerometer.watchAcceleration(this.onSuccess, onError, { frequency: 1000});
   },
-  representa: function(dato, elementoHTML) {
-      redondeo=Math.round(dato*100)/100;
-      document.querySelector(elementoHTML).innerHTML=redondeo;
+
+  onSuccess: function(datosAceleracion){
+    app.detectaAgitacion(datosAceleracion);
+    app.representaValores(datosAceleracion);
+  },
+
+  detectaAgitacion: function(datosAceleracion){
+    agitacionX = datosAceleracion.x > 10;
+    agitacionY = datosAceleracion.y > 10;
+
+    if (agitacionX || agitacionY){
+      document.body.className = 'agitado';
+    }else{
+      document.body.className = '';
+    }
+  },
+
+  representaValores: function(datosAceleracion){
+    app.representa(datosAceleracion.x, '#valorx');
+    app.representa(datosAceleracion.y, '#valory');
+    app.representa(datosAceleracion.z, '#valorz');
+  },
+
+  representa: function(dato, elementoHTML){
+    redondeo = Math.round(dato * 100) / 100
+    document.querySelector(elementoHTML).innerHTML= redondeo;
   }
-  
+
 };
 
-if ('addEventListener' in document) {
+if ('addEventListener'in document) {
     document.addEventListener('deviceready', function() {
         app.inicio();
-    }, false);
+  }, false);
 }
-
